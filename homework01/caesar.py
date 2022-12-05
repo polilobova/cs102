034ea6f1552e9b495fb9d3fd57a85f2ec3d668d6
Polina <368441@edu.itmo.ru>
2022-12-05 17:17:10 +0300

    the first lab

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,32 +1,94 @@
+length = 26
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
+
     """
+
     Encrypts plaintext using a Caesar cipher.
+
     >>> encrypt_caesar("PYTHON")
+
     'SBWKRQ'
+
     >>> encrypt_caesar("python")
+
     'sbwkrq'
+
     >>> encrypt_caesar("Python3.6")
+
     'Sbwkrq3.6'
+
     >>> encrypt_caesar("")
+
     ''
+
     """
+
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    for i in range(len(plaintext)):
+
+        char = plaintext[i]
+
+        if char.isupper():
+
+            ciphertext += chr((ord(char) + shift - ord("A")) % length + ord("A"))
+
+        elif char.islower():
+
+            ciphertext += chr((ord(char) + shift - ord("a")) % length + ord("a"))
+
+        else:
+
+            ciphertext += char
+
     return ciphertext
 
 
+
+
+
+
+
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
+
     """
+
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
+
     'PYTHON'
+
     >>> decrypt_caesar("sbwkrq")
+
     'python'
+
     >>> decrypt_caesar("Sbwkrq3.6")
+
     'Python3.6'
+
     >>> decrypt_caesar("")
+
     ''
+
     """
+
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    for i in range(len(ciphertext)):
+
+        char = ciphertext[i]
+
+        if char.isupper():
+
+            plaintext += chr((ord(char) - shift - ord("A")) % length + ord("A"))
+
+        elif char.islower():
+
+            plaintext += chr((ord(char) - shift - ord("a")) % length + ord("a"))
+
+        else:
+
+            plaintext += char
+
     return plaintext
