909ece39057f6cf990277ce56f94b2280c02f6c6
Polina <368441@edu.itmo.ru>
2022-12-16 16:24:41 +0300

    fixed rsa 2

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -12,6 +12,8 @@ def is_prime(num: int) -> bool:
     >>> is_prime(8)
     False
     """
+    if num <= 1 or (num > 2 and num % 2 == 0):
+        return False
     for delitel in range(3, int(num ** (1 / 2)) + 1, 2):
         if num % delitel == 0:
             return False
