c18bbb47b3e7cf6c1453ad5ff031f2b293307920
Polina <368441@edu.itmo.ru>
2023-03-31 13:52:38 +0300

    fixed_1

--- homework04/life_gui.py
+++ homework04/life_gui.py
@@ -5,10 +5,7 @@ from ui import UI
 
 
 class GUI(UI):
-
-    def __init__(
-                 self, life: GameOfLife, cell_size: int = 10,
-                 speed: int = 10) -> None:
+    def __init__(self, life: GameOfLife, cell_size: int = 10, speed: int = 10) -> None:
         super().__init__(life)
         self.cell_size = cell_size
         self.speed = speed
@@ -18,7 +15,7 @@ class GUI(UI):
         self.screen = pygame.display.set_mode(self.screen_size)
 
     def draw_lines(self) -> None:
-        """ Отрисовать сетку """
+        """Отрисовать сетку"""
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
@@ -32,11 +29,17 @@ class GUI(UI):
         for y in range(self.height):
             for x in range(self.width):
                 if grid[y][x] == 0:
-                    pygame.draw.rect(self.screen, pygame.Color("white"),
-                                     (self.cell_size * x, self.cell_size * y, self.cell_size, self.cell_size))
+                    pygame.draw.rect(
+                        self.screen,
+                        pygame.Color("white"),
+                        (self.cell_size * x, self.cell_size * y, self.cell_size, self.cell_size),
+                    )
                 else:
-                    pygame.draw.rect(self.screen, pygame.Color("green"),
-                                     (self.cell_size * x, self.cell_size * y, self.cell_size, self.cell_size))
+                    pygame.draw.rect(
+                        self.screen,
+                        pygame.Color("green"),
+                        (self.cell_size * x, self.cell_size * y, self.cell_size, self.cell_size),
+                    )
 
     def change_status(self) -> None:
         x, y = pygame.mouse.get_pos()
@@ -45,22 +48,17 @@ class GUI(UI):
         self.grid[row][col] = (self.grid[row][col] + 1) % 2
 
     def run(self) -> None:
-        """ Запустить игру """
+        """Запустить игру"""
         pygame.init()
         clock = pygame.time.Clock()
-        pygame.display.set_caption('Game of Life')
-        self.screen.fill(pygame.Color('white'))
+        pygame.display.set_caption("Game of Life")
+        self.screen.fill(pygame.Color("white"))
         self.grid = self.life.curr_generation
         running = True
         pause = False
-        while running and self.life.is_changing and not self.life.is_max_generations_exceeded:
-            for event.type in pygame.event.get():
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     life = GameOfLife((20, 30), True, max_generations=200)
     gui = GUI(life, 15, 15)
     gui.run()
