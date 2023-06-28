dd728551f05b61d7f53754664aa22ba71208807f
Polina <368441@edu.itmo.ru>
2023-06-28 09:08:11 +0300

    Code added

--- homework07/research/age.py
+++ homework07/research/age.py
@@ -2,7 +2,7 @@ import datetime as dt
 import statistics
 import typing as tp
 
-from vkapi.friends import get_friends
+from homework07.vkapi.friends import get_friends
 
 
 def age_predict(user_id: int) -> tp.Optional[float]:
@@ -14,4 +14,18 @@ def age_predict(user_id: int) -> tp.Optional[float]:
     :param user_id: Идентификатор пользователя.
     :return: Медианный возраст пользователя.
     """
-    pass
+    ages = []
+    friends = get_friends(user_id, fields=["bdate"])
+    for friend in friends.items:
+        try:
+            age = dt.datetime.today().year - int(friend["bdate"].split(".")[2])  # type: ignore
+            ages.append(age)
+        except:
+            pass
+    if ages:
+        return statistics.median(ages)
+    return None
+
+
+if __name__ == "__main__":
+    print("Предполагаемый возраст друзей пользователя: ", age_predict(560176709))
